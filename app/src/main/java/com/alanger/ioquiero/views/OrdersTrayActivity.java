eda54dbc5bd0833d3648279f9b38f5841c16a6c5
Alan Gerónimo <contacto@alangeronimo.com>
2019-07-18 18:40:53 -0500

    maquetado la bandeja e  item de  la bandeja

---
+++ app/src/main/java/com/alanger/ioquiero/views/OrdersTrayActivity.java
@@ -0,0 +1,19 @@
+package com.alanger.ioquiero.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alanger.ioquiero.R;
+
+public class OrdersTrayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_tray);
+
+
+
+    }
+}

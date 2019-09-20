c50b6c54851705fedeb39457223d9a2154f78096
Alan Gerónimo <alangeronimo@outlook.com>
2019-09-19 23:48:53 -0500

    terminado fragment temporal

--- app/src/main/java/com/alanger/ioquiero/getTariff/view/ActivityGetData.java
+++ app/src/main/java/com/alanger/ioquiero/getTariff/view/ActivityGetData.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.alanger.ioquiero.R;
+
 public class ActivityGetData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_data);
+        setContentView(R.layout.fragment_temporal);
     }
 }

7c38d4533ed7fdf31046c01fef3636294cb1db27
Ernesto Chero <informatico240@gmail.com>
2019-11-16 19:34:43 -0500

    Updated BASE_URL to volskayaforce-alfa

--- app/src/main/java/com/alanger/ioquiero/volskayaGraphql/GraphqlClient.java
+++ app/src/main/java/com/alanger/ioquiero/volskayaGraphql/GraphqlClient.java
@@ -1,17 +1,11 @@
 package com.alanger.ioquiero.volskayaGraphql;
 
-import com.alanger.ioquiero.type.CustomType;
 import com.apollographql.apollo.ApolloClient;
-import com.apollographql.apollo.response.CustomTypeAdapter;
-import com.apollographql.apollo.response.CustomTypeValue;
-
-import org.jetbrains.annotations.NotNull;
-
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class GraphqlClient {
-    private static final String BASE_URL = "https://volskayaforce.herokuapp.com/api/graphql";
+    private static final String BASE_URL = "https://volskayaforce-alfa.herokuapp.com/api/graphql";
     public static ApolloClient getMyApolloClient() {
 
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
